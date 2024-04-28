6836f236427f908a7efc6ed23352d803d7f2f8ec
KanuriSathvika <117982917+KanuriSathvika@users.noreply.github.com>
2024-04-28 21:59:44 +0530

    Create Delete Middle of Linked List.java

---
+++ Linked_List/Delete Middle of Linked List.java
@@ -0,0 +1,32 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    Node deleteMid(Node head) {
+        // This is method only submission.
+        // You only need to complete the method.
+        if(head==null){
+            return null;
+        }
+        
+        if(head!=null && head.next==null){
+            return null;
+        }
+        Node prev=head;
+        Node slow= head;
+        Node fast=head;
+        
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=slow.next;
+        return head;
+    }
+}
